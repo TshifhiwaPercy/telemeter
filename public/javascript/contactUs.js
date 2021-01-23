c4bc53c5511cf540c2d7ba41467e0b27e8f00a95
Tshifhiwa <tshifhiwamphoshomali@gmail.com>
2021-01-24 00:03:21 +0200

    version 1.0.1

--- public/javascript/contactUs.js
+++ public/javascript/contactUs.js
@@ -1,5 +1,20 @@
 var ltCount = 0;
 var wordCountInterval;
+function pageLoad(){
+	try{
+		const width = window.innerWidth;
+		const height = window.innerHeight;
+		var main_container = document.getElementById("main_container");
+		if(width >900){
+			main_container.style.height = (height*0.8)+"px";
+		}
+
+	}
+	catch(exception)
+	{
+		console.log(exception);
+	}
+}
 function letterCount(){
 	try{
 		
@@ -12,11 +27,18 @@ function letterCount(){
 }
 function countLetters(){
 	try{
-		
 		var txtArea= document.getElementById("txtArea");
 		var wordCounter = document.getElementById("letter_count");
-		console.log(txtArea.innerText.length);
+		
+		if(ltCount==250){
+			txtArea.readOnly = true;
+		}
+		else{
+		ltCount = (txtArea.value).length;
 		wordCounter.innerHTML = ltCount+"/250";
+
+		}
+		
 
 	}
 	catch(exception){
