16262847acd904893de5c5dc1667574c946fce2c
Tshifhiwa <tshifhiwamphoshomali@gmail.com>
2021-01-25 03:22:38 +0200

    version 1.0.2

--- public/javascript/contactUs.js
+++ public/javascript/contactUs.js
@@ -1,17 +1,64 @@
 var ltCount = 0;
+var showForm = true;
+var mobile = false; //Variable indicates whether user is using a mobile or pc
+var ran = false; 
 var wordCountInterval;
 function pageLoad(){
 	try{
+		
 		const width = window.innerWidth;
 		const height = window.innerHeight;
 		var main_container = document.getElementById("main_container");
 		if(width >900){
 			main_container.style.height = (height*0.8)+"px";
+			mobile = false;
+			showForm = false;
+			showMore();
+		}
+		else{
+			showForm = false;
+			mobile = true;
+			showMore();
 		}
 
 	}
 	catch(exception)
 	{
+		console.log(exception);
+	}
+}
+function showMore(){
+	try{
+		var form = document.getElementById("contact_form");
+		var details = document.getElementById("general_Information");
+			
+		if(mobile){
+			if(showForm){
+				showForm = false;
+				form.style.height = "0px";
+				form.style.visibility="hidden";
+				details.style.height = "auto";
+				details.style.visibility="visible";
+			}
+			else{
+				showForm = true;
+				form.style.height = "auto";
+				form.style.visibility = "visible";
+				details.style.height = "0px";
+				details.style.visibility = "hidden";
+			}
+		}
+		else{
+			form.style.height = "auto";
+			form.style.visibility = "visible";
+			details.style.height = "auto";
+			details.style.visibility = "visible";
+			details.style.bottom = "0";
+		}
+
+
+	}
+	catch(exception){
 		console.log(exception);
 	}
 }
